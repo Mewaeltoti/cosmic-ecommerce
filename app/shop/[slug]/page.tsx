83d9735568d5cfa7dfaefa40d61f055247ff43ac
Mewaeltoti <mewaeltoti@gmail.com>
2025-04-06 10:39:10 +0300

    g

--- app/shop/[slug]/page.tsx
+++ app/shop/[slug]/page.tsx
@@ -1,18 +1,15 @@
-// app/shop/[slug]/page.tsx
-import { SingleProduct } from "@/cosmic/blocks/ecommerce/SingleProduct"
-export default async function SingleProductPage({
-  params,
-  searchParams
-}: {
-    params: { slug: string }
-    searchParams: {
-      success?: string
-    }
-  }) {
+import { SingleProduct } from "@/cosmic/blocks/ecommerce/SingleProduct";
+
+type Props = {
+  params: { slug: string };
+  searchParams: { success?: string };
+};
+
+export default async function SingleProductPage({ params, searchParams }: Props) {
   return (
     <SingleProduct
       query={{ slug: params.slug, type: "products" }}
-      purchased={searchParams.success ? true : false}
+      purchased={!!searchParams.success}
     />
   );
 }
