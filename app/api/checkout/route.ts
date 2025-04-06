3e2bb69ea9132c462109b84d0e8a4017e335b615
Mewaeltoti <mewaeltoti@gmail.com>
2025-04-06 10:20:34 +0300

    f

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,4 +1,5 @@
 // app/api/checkout/route.ts
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 import { type NextRequest, NextResponse } from "next/server";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
